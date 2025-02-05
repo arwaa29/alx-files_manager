1b01b6f6f8ae8dac928ecf5f0458cc8adaff4135
arwaa29 <arwahesham73@gmail.com>
2025-02-05 14:10:55 +0200

    file manager

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -5,13 +5,13 @@ class AppController {
   static getStatus(req, res) {
     const redisLive = redisClient.isAlive();
     const dbLive = dbClient.isAlive();
-    res.status(200).json({ "redis": true, "db": true });
+    res.status(200).json({ redis: redisLive, db: dbLive });
   }
 
   static async getStats(req, res) {
     const usersTotal = await dbClient.nbUsers();
     const filesTotal = await dbClient.nbFiles();
-    res.status(200).json({ "users": 12, "files": 1231 });
+    res.status(200).json({ users: usersTotal, files: filesTotal });
   }
 }
 
